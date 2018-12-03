dbcf64e0d04a49ef7876e8b3523ee214963ee738
poonam.jondhale <poonam.jondhale@globant.com>
2018-12-03 13:49:59 +0530

    commit

---
+++ Poonam/FirstApp/ConsoleApp1/Add.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+
+{
+    class Add
+    {
+        static void Main12(string[] args)
+        {
+         
+            Console.WriteLine("Enter value of a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of b");
+            int b = Int32.Parse(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("ADDITITON IS " + c);
+            Console.ReadLine();
+        }
+    }
+}
