772b39056ec6a9333f0208aaa825b8f19567ad3c
neeraj.goidani <neeraj.goidani@globant.com>
2018-12-05 12:35:51 +0530

    Done with ToDo assignment (logging inside it) and done with Exception Handling Assignment(Logging inside this too)

--- Neeraj/HelloWorld/CollectionsToDoList/Program.cs
+++ Neeraj/HelloWorld/CollectionsToDoList/Program.cs
@@ -1,13 +1,19 @@
+using log4net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CollectionsToDoList
 {
     class Program
     {
+        //Declare an instance for log4net
+        private static readonly ILog Log =
+              LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
             List<ToDo> toDoList = new List<ToDo>();
@@ -87,9 +93,43 @@ namespace CollectionsToDoList
 
                 }
             } while (choice != 5);
-            
 
+            ImplementLoggingFuntion();
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+
+        private static void ImplementLoggingFuntion()
+        {
+            /* We have 5 levels of log message. Let's test all.
+             *  FATAL
+                ERROR
+                WARN
+                INFO
+                DEBUG
+             */
+            var secs = 3;
+            Log.Fatal("Start log FATAL...");
+            Console.WriteLine("Start log FATAL...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Error("Start log ERROR...");
+            Console.WriteLine("Start log ERROR...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Warn("Start log WARN...");
+            Console.WriteLine("Start log WARN...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Info("Start log INFO...");
+            Console.WriteLine("Start log INFO...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Debug("Start log DEBUG...");
+            Console.WriteLine("Start log DEBUG...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Console.WriteLine("Press any key to close the application");
+            Console.ReadKey();
         }
     }
 }
