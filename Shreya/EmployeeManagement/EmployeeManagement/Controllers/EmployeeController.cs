26be5739aa01990dce32f73a57e8b3839e8cf83c
shreya.roy <shreya.roy@globant.com>
2018-12-18 12:30:20 +0530

    Employee Management MVC Application

---
+++ Shreya/EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,65 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        List<Employee> employee = new List<Employee>();
+        Hr hr = new Hr();
+        
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello";
+            return View(employee);
+        }
+
+        public ActionResult getEmployeeList()
+        {
+            hr.getList(getSalary);
+            return View(employee);
+        }
+
+        public void getSalary(List<Employee> emp)
+        {
+            employee = emp;
+        }
+
+        public ActionResult getHighestSalary()
+        {
+            List<Employee> emp = hr.employeeList();
+            IEnumerable<Employee> employee = emp.OrderByDescending(x => x.EmpSalary).Take(1);
+            return View(employee);
+        }
+       
+    }
+
+    public class Hr
+    {
+        public delegate void functionptr(List<Employee> emp);
+        public void getList(functionptr obj)
+        {
+            List<Employee> employees = new List<Employee>();
+            employees = employeeList();
+            obj(employees);
+        }
+
+        public List<Employee> employeeList()
+        {
+            List<Employee> employee = new List<Employee>()
+            {
+                new Employee(1,"Shreya","Developer",30000),
+                new Employee(2,"Vinit" , "Testing" , 20000),
+                new Employee(3,"Akash" , "QA" , 4500),
+            };
+            return employee;
+        }
+    }
+}
---
+++ Shreya/EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public String EmpName { get; set; }
+        public String EmpDept { get; set; }
+        public int EmpSalary { get; set; }
+
+        public Employee()
+        {
+
+        }
+        public Employee(int EmpId, String EmpName, String EmpDept, int EmpSalary)
+        {
+            this.EmpId = EmpId;
+            this.EmpName = EmpName;
+            this.EmpDept = EmpDept;
+            this.EmpSalary = EmpSalary;
+        }
+    }
+}
