ef2d83e0d67e7287a9690029c22906ff4ba044fe
poonam.jondhale <poonam.jondhale@globant.com>
2018-12-20 11:42:03 +0530

    commit

---
+++ Poonam/WebApiTodo/WebApiTodo/Controllers/TodoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiTodo.Dao;
+using WebApiTodo.Models;
+
+namespace WebApiTodo.Controllers
+{
+    public class TodoController : ApiController
+    {
+        TodoDao dao = new TodoDao();
+      
+        public List<Todo> GetAllTodos()
+        {
+            return dao.GetTodoList();
+        }
+
+        public Todo Get(int id)
+        {
+           return dao.GetTodoById(id);
+        }
+
+        public void Post([FromBody]Todo todo)
+        {
+            dao.InsertInTodo(todo);
+        }
+
+        public void Put(int id, [FromBody]Todo todo)
+        {
+         dao.UpdateById(id, todo);
+        }
+       
+        public void Delete(int id)
+        {
+            dao.DeleteById(id);
+        }
+    }
+}
---
+++ Poonam/WebApiTodo/WebApiTodo/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiTodo.Models
+{
+    public class Todo
+    {
+        public int TaskId { set; get; }
+        public string TaskName { set; get; }
+        public Todo(int TaskId,string TaskName)
+        {
+            this.TaskId = TaskId;
+            this.TaskName = TaskName;
+        }
+    }
+}
