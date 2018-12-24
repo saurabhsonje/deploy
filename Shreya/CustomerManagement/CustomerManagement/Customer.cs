f5382f971e4b9328cdb729e8a03e4bfc11067ebd
shreya.roy <shreya.roy@globant.com>
2018-12-24 12:18:28 +0530

    Customer Management Assignment

---
+++ Shreya/CustomerManagement/CustomerManagement/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement
+{
+    class Customer 
+    {
+        private FileLogger obj = new FileLogger();
+
+        public virtual double getDiscount(double TotalSales)
+        {
+            return TotalSales;
+        }
+    }
+}
---
+++ Shreya/CustomerManagement/CustomerManagement/GoldCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement
+{
+    class GoldCustomer : Customer
+    {
+        public override double getDiscount(double TotalSales)
+        {
+            Console.WriteLine("Congrats Customer!! you get Discount of 15%");
+            return base.getDiscount(TotalSales) - 100;
+        }
+    }
+}
