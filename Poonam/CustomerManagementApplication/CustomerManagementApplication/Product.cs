0f6cde0e044028bd5d6e0650de18d6911b51feff
poonam.jondhale <poonam.jondhale@globant.com>
2018-12-24 11:43:10 +0530

    commit

---
+++ Poonam/CustomerManagementApplication/CustomerManagementApplication/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagementApplication
+{
+    class Product
+    {
+        public string name { set; get; }
+        public double price { set; get; }
+        public Product()
+        {
+
+        }
+     public Product(string name,double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+        public List<Product> getAllProduct()
+        {
+            List<Product> product = new List<Product>();
+            product.Add(new Product("biscuits", 12));
+            product.Add(new Product("pen", 10));
+            product.Add(new Product("eraser", 5));
+            return product;
+        }
+    }
+
+}
---
+++ Poonam/CustomerManagementApplication/CustomerManagementApplication/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagementApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            List<Product> products = new List<Product>();
+            char ch;
+            do
+            {
+                FileLogger log = new FileLogger();
+                Console.WriteLine("1.Enquiry");
+                Console.WriteLine("2.Check Discount As Per Type Of Customer");
+                int choice;
+                Console.WriteLine("Enter Your Choice");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        products= product.getAllProduct();
+                        products.ForEach(x => Console.WriteLine(x.name+" "+x.price));
+                        Console.WriteLine("Visited For The Fisrt Time");
+                        log.handle("Logged in for first time");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Type Of Customer");
+                        string customer = Console.ReadLine();
+                        if(customer.Equals("Silver")||customer.Equals("silver"))
+                        {
+                           SilverCustomer silver = new SilverCustomer();
+                           double discount=silver.getDiscount();
+                           Console.WriteLine("Silver Customer Will Get this Discount " + discount);
+                           log.handle("Silver Customer Will Get this Discount " + discount);
+                        }
+                        if(customer.Equals("Gold")||customer.Equals("gold"))
+                        {
+                            GoldCustomer gold = new GoldCustomer();
+                            double discount = gold.getDiscount();
+                            Console.WriteLine("Gold Customer Will Get this Discount " + discount);
+                            log.handle("Gold Customer Will Get this Discount " + discount);
+                        }
+                        break;
+
+                  
+
+                }
+                Console.WriteLine("Do You Want To Continue");
+                ch = Convert.ToChar(Console.ReadLine());
+            } while (ch.Equals('y') || ch.Equals('Y'));
+
+
+        }
+    }
+    }
