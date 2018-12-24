8d77e5f50a2be6ba5828f24f7354a15dc9deaacf
neeraj.goidani <neeraj.goidani@globant.com>
2018-12-24 11:57:44 +0530

    MVC Web API updated

---
+++ Neeraj/HelloWorld/MvcWebAppTest1/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebAppTest1.Models
+{
+    public class Employee
+    {
+        public int age { get; set; }
+        public string name { get; set; }
+
+        public Employee(string name,int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+
+    }
+}
