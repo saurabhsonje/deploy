c76ae49feae0c00c0e434f99d0da3410abe3aa6f
neeraj.goidani <neeraj.goidani@globant.com>
2018-12-24 18:16:50 +0530

    Added class library - EmployeeDb. Added Crud Api with Database Connection. Need both these files together on system to run

---
+++ Neeraj/HelloWorld/CrudWebApiWithDb/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace CrudWebApiWithDb.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Neeraj/HelloWorld/CrudWebApiWithDb/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeDb;
+
+namespace CrudWebApiWithDb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+
+        public async Task<ActionResult> getAllEmployees()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:61482/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage responseMessage = await client.GetAsync("api/values");
+
+           
+            if (responseMessage.IsSuccessStatusCode == true)
+            {
+                List<tblEmployee> employeeList = await responseMessage.Content.ReadAsAsync<List<tblEmployee>>();
+
+                return View(employeeList);
+            }
+            else
+            {
+                return null;
+            }
+
+
+
+        }
+
+
+    }
+}
