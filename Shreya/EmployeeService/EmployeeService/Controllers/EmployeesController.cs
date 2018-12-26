6771e5eb51b233fe7b572a34adc28dc5c01a99d8
shreya.roy <shreya.roy@globant.com>
2018-12-26 14:18:40 +0530

    Employee Service using MVC+API and EF6

---
+++ Shreya/EmployeeService/EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<Employee> employee = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:56206/api/");
+
+                var responseTask = client.GetAsync("employeees/");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<List<Employee>>();
+                    readTask.Wait();
+
+                    employee = readTask.Result;
+                }
+                else
+                {
+                    employee = Enumerable.Empty<Employee>();
+                    ModelState.AddModelError(string.Empty, "Server error.Please contact administrator.");
+                }
+            }
+            return View(employee);
+        }
+
+        public ActionResult create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult create(Employee employee)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:56206/api/employees");
+                var postTask = client.PostAsJsonAsync<Employee>("employeees", employee);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error . Please contact administrator");
+
+            return View(employee);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Employee employee = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:56206/api/");
+                var responseTask = client.GetAsync("employeees?id=" + id.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<Employee>();
+                    readTask.Wait();
+
+                    employee = readTask.Result;
+                }
+            }
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:56206/api/employeees");
+
+                var putTask = client.PutAsJsonAsync<Employee>("employeees", employee);
+                putTask.Wait();
+
+                var result = putTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(employee);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:56206/api/");
+
+                var deleteTask = client.DeleteAsync("employeees/" + id.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+             
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Shreya/EmployeeService/EmployeeService/Controllers/api/EmployeeesController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id)  ;
+            }
+
+        }
+
+        public IHttpActionResult Post(Employee emp)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                entities.Employees.Add(new Employee()
+                {
+                    ID = emp.ID,
+                    FirstName = emp.FirstName,
+                    LastName = emp.LastName,
+                    Salary = emp.Salary,
+                    Department = emp.Department
+
+                });
+
+                entities.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(Employee emp)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var existingEmp = entities.Employees.Where(e => e.ID == emp.ID).FirstOrDefault<Employee>();
+                if (existingEmp != null)
+                {
+                    existingEmp.FirstName = emp.FirstName;
+                    existingEmp.LastName = emp.LastName;
+                    existingEmp.Department = emp.Department;
+
+                    entities.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            if (id <= 0)
+                return BadRequest("Not a valid Employee ID");
+
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var emp = entities.Employees.Where(e => e.ID == id).FirstOrDefault();
+                entities.Entry(emp).State = System.Data.Entity.EntityState.Deleted;
+                entities.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
