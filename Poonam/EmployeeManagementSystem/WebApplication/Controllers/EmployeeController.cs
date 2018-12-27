b8c5895a2219c896fcacc0df4be2f349e31e50e5
poonam.jondhale <poonam.jondhale@globant.com>
2018-12-27 12:45:23 +0530

    commit

---
+++ Poonam/EmployeeManagementSystem/WebApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication.DAO;
+namespace WebApplication.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+
+        EmployeeDao dao = new EmployeeDao();
+        [HttpPost]
+        public IHttpActionResult AddEmployee(EmployeeInformation employee)
+        {
+            if (employee == null)
+            {
+                var resp = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent(string.Format("No data present in the header")),
+                    ReasonPhrase = "Not Found Data in the Header"
+                };
+                throw new HttpResponseException(resp);
+
+            }
+            if (dao.AddEmployee(employee))
+            {
+                return Ok(employee);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        /* [HttpGet]
+         public List<EmployeeInformation> GetEmployees()
+         {
+             return dao.GetEmployees();
+         }
+         */
+
+        public IHttpActionResult GetEmployees()
+        {
+
+            List<EmployeeInformation> employees = dao.GetEmployees();
+
+            if (employees != null)
+            {
+                return Ok(employees);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+    }
+}
---
+++ Poonam/EmployeeManagementSystem/WebApplication/DAO/EmployeeDao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.DAO
+{
+    public class EmployeeDao
+    {
+        EmployeeDbEntities2 db = new EmployeeDbEntities2();
+        public bool AddEmployee(EmployeeInformation employee)
+        {
+            try
+            {
+                if (db.StudioInformations.Any(x => x.EmpStudio == employee.EmpStudio))
+                {
+                    db.EmployeeInformations.Add(employee);
+                    db.SaveChanges();
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+
+
+                
+            }
+            catch (Exception e)
+            {
+                
+                throw new Exception("There was a problem saving this record: " + e.Message);
+            }
+
+        }
+        public List<EmployeeInformation> GetEmployees()
+        {
+            return db.EmployeeInformations.ToList();
+        }
+    }
+}
