ecf2e2cc22d0f357423af8fa1f154ede1b18fef7
poonam.jondhale <poonam.jondhale@globant.com>
2018-12-27 12:37:20 +0530

    first commit

---
+++ FinalAssignment/EmployeeManagementSystem/WebApplication/Controllers/EmployeeClientController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.HttpClients;
+namespace WebApplication.Controllers
+{
+    public class EmployeeClientController : Controller
+    {
+        EmployeeClient client = new EmployeeClient();
+        // GET: EmployeeClient
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeInformation employee)
+        {
+           if( client.Create(employee))
+            return RedirectToAction("GetEmployees");
+           else
+                return RedirectToAction("Create");
+        }
+        [HttpGet]
+        public ActionResult GetEmployees()
+        {
+            List<EmployeeInformation> employees=client.GetEmployees();
+            return View(employees);
+        }
+    }
+}
---
+++ FinalAssignment/EmployeeManagementSystem/WebApplication/HttpClients/EmployeeClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace WebApplication.HttpClients
+{
+    public class EmployeeClient
+    {
+        public bool Create(EmployeeInformation employee)
+        {
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:54297/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.PostAsJsonAsync("api/employee/", employee).Result;
+                bool xyz = response.IsSuccessStatusCode;
+                 return xyz;
+              
+            }
+            catch (Exception exe)
+            {
+                return false;
+            }
+
+        }
+        public List<EmployeeInformation> GetEmployees()
+        {
+            
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:54297/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync("api/employee/").Result;
+                if (response.IsSuccessStatusCode == true)
+                {
+                    List<EmployeeInformation> readResult = response.Content.ReadAsAsync<List<EmployeeInformation>>().Result;
+                    return readResult;
+                }
+                else
+                {
+                    return null;
+                }
+
+           
+
+                
+            }
+            catch (Exception exe)
+            {
+                return null;
+            }
+
+        }
+    }
+}
