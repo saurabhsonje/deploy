82ce65ca3a6f9ef45d7ee55aad9a3958b94f9e4c
poonam.jondhale <poonam.jondhale@globant.com>
2018-12-27 18:22:45 +0530

    1st Commit

---
+++ Poonam/FinalAssignment/EmployeeManagementSystem/Controllers/EmployeeMvcController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class EmployeeMvcController : Controller
+    {
+        // GET: EmployeeMvc
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:58019/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.PostAsJsonAsync("api/employeeapi/", employee).Result;
+                bool xyz = response.IsSuccessStatusCode;
+                if(xyz==true)
+                {
+                    return RedirectToAction("Sucess");
+                }
+            }
+            catch (Exception exe)
+            {
+                return View();
+            }
+            return View();
+        }
+        public ActionResult Sucess()
+        {
+            return View();
+        }
+    }
+}
---
+++ Poonam/FinalAssignment/EmployeeManagementSystem/DAO/EmployeeDao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagementSystem.DAO
+{
+    public class EmployeeDao
+    {
+        EmployeeDataEntities db = new EmployeeDataEntities();
+        public bool AddEmployee(Employee employee)
+        {
+            try
+            {
+                if (db.Studios.Any(x => x.EmpStudio == employee.EmpStudio))
+                {
+                    db.Employees.Add(employee);
+                    db.SaveChanges();
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("There was a problem saving this record: " + e.Message);
+            }
+
+        }
+        public List<Employee> GetEmployees()
+        {
+            return db.Employees.ToList();
+        }
+    }
+}
