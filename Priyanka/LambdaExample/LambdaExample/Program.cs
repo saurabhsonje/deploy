81bd3ae0ba42c32603cc077878d87ffb0bda7938
priyanka.thakur <priyanka.thakur@globant.com>
2019-03-13 16:24:15 +0530

    Checking in the changes for the first project

---
+++ Priyanka/LambdaExample/LambdaExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System;
+
+namespace LambdaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] a = { 40, 50, 10, 20, 30, 70, 100 };
+            int display = a.Select(x =>x).Sum();
+            Console.WriteLine("Addition using Lambda Expression: " + display);
+           
+
+            var sort = a.OrderBy(x => x);
+            Console.WriteLine("Sorting using Lambda Expression: ");
+            foreach (var s in sort)
+            {
+                Console.WriteLine(s);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
