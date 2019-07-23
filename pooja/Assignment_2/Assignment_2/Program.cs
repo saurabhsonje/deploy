0250b8bc90519fe1dcbb545d7b370389c2cd4cd7
Pooja Yerva <yerva.pooja@outlook.com>
2019-07-23 18:15:41 +0530

    Day 2 Assignments

---
+++ pooja/Assignment_2/Assignment_2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle obj = new Circle();
+            // obj.CalculateArea();
+
+
+            Circle obj1 = new Circle(2);
+            obj1.CalculateArea();
+
+            Employee e = new Employee();
+            Console.WriteLine("FullName=={0}", e.FullName());
+
+            Employee emp = new Employee("Pooja", "Yerva");
+            Console.WriteLine("fullName=={0}", emp.FullName());
+
+            Console.ReadLine();
+        }
+
+    }
+
+    public class Circle
+    {
+        static float _Pi;
+        int _Radius;
+
+
+
+        static Circle()
+        {
+            Console.WriteLine("from Static Constructor");
+            _Pi = 3.141F;
+        }
+
+        public Circle()
+        {
+            Console.WriteLine("From Default Constructor");
+        }
+
+        public Circle(int radius)
+        {
+            Console.WriteLine("From Paramaterized Constructor");
+            this._Radius = radius;
+        }
+
+
+        public void CalculateArea()
+        {
+
+            float Area = this._Radius * this._Radius * Circle._Pi;
+            Console.WriteLine("area {0}", Area);
+        }
+
+        ~Circle()
+        {
+            // Destructor.
+        }
+
+    }
+
+    public class Employee
+    {
+        string _FName;
+        String _LName;
+
+
+        public Employee() : this("FName", "LName")
+        {
+
+            Console.WriteLine("From Default Constructor from Employee");
+        }
+
+        public Employee(string FName, string LName)
+        {
+            Console.WriteLine("Parameterised Constructor From Employee");
+            this._FName = FName;
+            this._LName = LName;
+        }
+
+        public string FullName()
+        {
+            String Full_Name = this._FName + this._LName;
+            return Full_Name;
+        }
+
+
+    }
+}
+
+
---
+++ pooja/Assignment_2/Assignment_2c/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2c
+{
+
+
+    class Generic_Program<T>
+    {
+        public List<T> _List = new List<T>();
+
+        public static void Print(List<Employee> pList, string info)
+        {
+            try
+            {
+                Console.WriteLine(info);
+
+                pList.ForEach(delegate (Employee per)
+                {
+                    Console.WriteLine(Convert.ToString(per.ID) + "  " + per.FirstName + "  " + per.MiddleName + "  " + per.LastName);
+                });
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+
+            }
+            Console.ReadLine();
+
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generic Type :List
+            Generic_Program<Employee> Employee = new Generic_Program<Employee>();
+            Employee._List.Add(new Employee(1, "Pooja", "A", "yerva"));
+            Employee._List.Add(new Employee(2, "Rupali", "A", "More"));
+            Employee._List.Add(new Employee(3, "Shubham", "A", "Yadav"));
+            Employee._List.Add(new Employee(4, "Mukesh", "A", "Jha"));
+
+
+            Generic_Program<Employee>.Print(Employee._List, "Display all items in the List");
+
+            //Add Employee 
+            Generic_Program<Employee> newList = new Generic_Program<Employee>();
+            Employee._List.Add(new Employee(5, "ABC", "B", "DEF"));
+            Employee._List.Add(new Employee(6, "XYZ", "B", "PQR"));
+            Employee._List.AddRange(newList._List);
+
+            Generic_Program<Employee>.Print(Employee._List, "Add new List<T> to existing List");
+
+
+            //Delete Employee 
+            //  Generic_Program<Employee> removeListItem = new Generic_Program<Employee>();
+            Employee._List.RemoveAt(1);
+
+            Generic_Program<Employee>.Print(Employee._List, "Remove multiple items from List<> based on condition ");
+
+        }
+
+    }
+
+
+}
+
+
+
