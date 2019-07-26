0ea1b7150e00539972d9b76beffcece6c2cfd4a8
Pooja <pooja.yerva@globant.com>
2019-07-26 11:36:31 +0530

    .Net Assignment_Day 04

---
+++ pooja/Assignment_4/Assignment4b/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4b
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string  Location { get; set; }
+
+        public Student(int id, string name, string location)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Location = location;
+
+        }
+    }
+}
---
+++ pooja/Assignment_4/Assignment_4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Any String");
+            string str = Console.ReadLine(); 
+            printCharWithFreq(str);
+            Console.ReadKey();
+        }
+     
+        static void printCharWithFreq(String str)
+        {
+            // size of the string 'str' 
+            int n = str.Length;
+          
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (str[i] == str[j])
+                        count++;
+                }
+                if (!dict.ContainsKey(str[i]))
+
+                    dict.Add(str[i], count);
+            }
+            for(int i=0;i<dict.Count;i++)
+            {
+
+                Console.WriteLine("{0}{1}",dict.Keys.ElementAt(i),dict[dict.Keys.ElementAt(i)]);
+                
+            }
+         
+        }
+    }
+}
+
+
+
