1f5cfa102ae8f5e066f3ad0c3781589b32be87c3
Pooja <pooja.yerva@globant.com>
2019-07-29 11:30:16 +0530

    .Net Assignments

---
+++ pooja/Assignment5/Assignment5/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+        public int Salary { get; set; }
+
+        public Employee()
+        {
+
+        }
+        public Employee(int id, string name,string location, int salary)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Location = location;
+            this.Salary = salary;
+        }
+
+
+    }
+}
---
+++ pooja/Assignment5/Assignment5b/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char Inputchoice = 'N';
+            int InputFromUser = 0;
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date);
+
+           
+            do
+            {
+                Console.WriteLine("---------------------");
+                Console.WriteLine("1.DateWithDayPrefix");
+                Console.WriteLine("2. DateWithFullDayPrefix");
+                Console.WriteLine("3.DateWithTime");
+                Console.WriteLine("4.DateTimeStamp");
+                Console.WriteLine("5.DateJson");
+                Console.WriteLine("6.DateTimeJson");
+                Console.WriteLine("7.DateLocal");
+                Console.WriteLine("Enter Your Choice....!!!");
+                Console.WriteLine("---------------------");
+
+                InputFromUser = Convert.ToInt32(Console.ReadLine());
+                string date1 = date.DisplayDate(Convert.ToString(InputFromUser));
+                Console.WriteLine(date1);
+
+                Console.WriteLine("Do you want to continue(Y/N)");
+                Inputchoice = Convert.ToChar(Console.ReadLine());
+
+            } while (Inputchoice.Equals('Y') || (Inputchoice.Equals('y')));
+            
+           
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
