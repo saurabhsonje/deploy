88c662bc8043ec550ba5eb381b026b66bd890352
Shubham Yadav <shubhamyadav.globant@outlook.com>
2019-08-09 17:39:03 +0530

    Committing changes related Assignment related to JWT.

---
+++ SHUBHAM/JWT_TOKEN/JWT_TOKEN/Providers/ExtendedClaimsProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace JWT_TOKEN.Providers
+{
+    public static class ExtendedClaimsProvider
+    {
+       
+        public static Claim CreateClaim(string type, string value)
+        {
+            return new Claim(type, value, ClaimValueTypes.String);
+        }
+
+    }
+}
