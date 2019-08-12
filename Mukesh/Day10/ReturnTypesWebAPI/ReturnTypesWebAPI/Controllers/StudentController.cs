470713ee12fed33107944da4d55c6f3cabf0ebf6
Shubham Yadav <shubhamyadav.globant@outlook.com>
2019-08-12 14:37:26 +0530

    Done Changes

---
+++ Mukesh/Day10/ReturnTypesWebAPI/ReturnTypesWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ReturnTypesWebAPI.Models;
+
+namespace ReturnTypesWebAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        static List<Student> students = new List<Student>() {
+            new Student{ID=10,Name="Rajesh",Branch="IT"},
+            new Student{ID=12,Name="Deepti",Branch="Comp"},
+            new Student{ID=14,Name="Sanket",Branch="Mech"}
+        };
+
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, students);
+        }
+
+        public IHttpActionResult GetByID(int id)
+        {
+            var student = students.FirstOrDefault(s=>s.ID==id);
+            if (student == null)
+            {
+                //Alternatively we can use return Notfound()-->But it won't show msgs
+                return Content(HttpStatusCode.NotFound,"Sudent Details not found");
+            }
+
+            return Ok(student);
+        }
+
+        [Route("api/GetStudentByObject/{name}")]
+        public Student GetStudentByObject(string name)
+        {
+            var studentByName = students.FirstOrDefault(s => s.Name == name);
+
+            return studentByName;
+        }
+
+        [Route("api/GetStudentByName/{name}")]
+        public string GetStudentByName(string name)
+        {
+            var studentByName = students.FirstOrDefault(s => s.Name == name);
+
+            return studentByName.Name ;
+        }
+
+
+
+        public void Post([FromBody] Student value)
+        {
+            students.Add(value);
+        }
+    }
+}
---
+++ Mukesh/Day11/WebAPIAuthorization/WebAPIAuthorization/Migrations/Configuration.cs
@@ -0,0 +1,61 @@
+namespace WebAPIAuthorization.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using WebAPIAuthorization.Infrastructure;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebAPIAuthorization.Infrastructure.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebAPIAuthorization.Infrastructure.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+
+            var user = new ApplicationUser()
+            {
+                UserName = "SuperPowerUser",
+                Email = "taiseer.joudeh@gmail.com",
+                EmailConfirmed = true,
+                FirstName = "Taiseer",
+                LastName = "Joudeh",
+                Level = 1,
+                JoinDate = DateTime.Now.AddYears(-3)
+            };
+
+            manager.Create(user, "MySuperP@ss!");
+
+            if (roleManager.Roles.Count() == 0)
+            {
+                roleManager.Create(new IdentityRole { Name = "SuperAdmin" });
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
+
+            var adminUser = manager.FindByName("SuperPowerUser");
+
+            manager.AddToRoles(adminUser.Id, new string[] { "SuperAdmin", "Admin" });
+        }
+    }
+}
