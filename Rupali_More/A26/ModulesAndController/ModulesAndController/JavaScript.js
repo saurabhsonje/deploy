93d0c14428ed02491b009687134a842f563a8fc4
Rupali More <rupali.more@globant.com>
2019-08-21 12:54:12 +0530

    dot net assignment

---
+++ Rupali_More/A26/ModulesAndController/ModulesAndController/JavaScript.js
@@ -0,0 +1,17 @@
+﻿var Addition = angular.module('Addition', []);
+Addition.controller('additioncntrl', function ($scope) {
+    $scope.a = 10;
+    $scope.b = 10;
+    $scope.c = $scope.a + $scope.b;
+});
+
+
+var subtraction = angular.module('subtraction', []);
+subtraction.controller('subtractioncntrl', function ($scope) {
+    $scope.a1 = 20;
+    $scope.b1 = 10;
+    $scope.d = $scope.a1 - $scope.b1;
+});
+
+
+angular.module("CombineModule", ["Addition", "subtraction"]);
---
+++ Rupali_More/A26/modulesAndController2/modulesAndController2/JavaScript.js
@@ -0,0 +1,36 @@
+﻿var Addition = angular.module('Addition', []);
+Addition.controller('additioncntrl', function ($rootScope , $scope) {
+    $rootScope.a ;
+    $rootScope.b;
+    $scope.Add = function () {
+        $scope.c = $rootScope.a + $rootScope.b;
+    };
+});
+
+
+var substraction = angular.module('substraction', []);
+substraction.controller('substractioncntrl', function ($rootScope,$scope) {
+    $scope.Sub = function () {
+        $scope.d = $rootScope.a - $rootScope.b;
+    };
+});
+
+var multiplication = angular.module('multiplication', []);
+multiplication.controller('multiplicationcntrl', function ($rootScope, $scope)
+{
+    $scope.Mul = function () {
+        $scope.e = $rootScope.a * $rootScope.b;
+    };
+    });
+
+
+
+var Division = angular.module('Division', []);
+Division.controller('Divisioncntrl', function ($rootScope, $scope) {
+    $scope.Div = function () {
+        $scope.f = $rootScope.a / $rootScope.b;
+    };
+});
+
+
+var Mainapp = angular.module("CombineModule", ["Addition", "substraction", "multiplication","Division"]);
