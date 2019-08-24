409e4918da1840a174ccea632159fbead270945d
Pooja <pooja.yerva@globant.com>
2019-08-24 20:01:50 +0530

    Angular Assignments

---
+++ pooja/Angular_Assignments/Assignment_A28(WEBAPI_part)/Assignment_A28(WEBAPI_part)/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Assignment_A28_WEBAPI_part_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_A28_WEBAPI_part_.Controllers
+{
+    public class HomeController : ApiController
+    {
+        static List<Employee> employees = new List<Employee>()
+        {
+            new Employee(2, "Pooja", 30000, "solapur"),
+            new Employee(1, "Rupali", 35000, "shahada"),
+            new Employee(3, "Mukesh", 40000, "surat"),
+            new Employee(4, "Shubham", 45000, "Lacknow")
+        };
+
+        [HttpGet]
+        [ActionName("GetInfo")]
+        public List<Employee> EmployeeList()
+        {
+            return employees;
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public IHttpActionResult PostEmployeeInfo([FromBody]Employee employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest("CAN'T BE NULL");
+            }
+            employees.Add(employee);
+            employees = employees.ToList();
+            return Ok();
+
+        }
+    }
+}
---
+++ pooja/Angular_Assignments/Assignment_A28(WEBAPI_part)/Assignment_A28(WEBAPI_part)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment_A28_WEBAPI_part_.WebApiApplication" Language="C#" %>
