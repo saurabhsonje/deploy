dd45e8f966d3426148451ee771beb1dbe7f2316e
Pooja <pooja.yerva@globant.com>
2019-08-26 11:48:35 +0530

    Angular Assignment

---
+++ pooja/Angular_Assignments/Assignment_A30(angular_part)/Assignment_A30(angular_part)/script1.js
@@ -0,0 +1,50 @@
+﻿var app = angular.module('appServices', []);
+
+app.controller("angularController", function ($scope, $http, $log) {
+
+    $scope.AddEmployee = function (emp) {
+
+        emp.Id = $scope.employees.length + 1;
+
+        $http({
+
+            method: 'Post',
+            url: 'http://localhost:53497/api/home/EmployeeInfo',
+            data: emp
+        })
+            .then(function (response) {
+                $scope.employees = response.data;
+                $log.info(response);
+                $scope.GetEmpInfo();
+            }, function (reason) {
+                $scope.error = reason.data;
+                $log.info(reason);
+            })
+            .catch(function (e) {
+                console.log('Error: ', e);
+                throw e;
+            });
+
+    };
+
+
+    $scope.GetEmpInfo = function () {
+        $http({
+
+            method: 'Get',
+            url: 'http://localhost:53497/api/home/GetInfo'
+        })
+        .then(function (response) {
+            $scope.employees = response.data;
+
+            $log.info($scope.employees);
+        }, function (reason) {
+            $scope.error = reason.data;
+            $log.info(reason);
+        });
+
+
+    }
+
+});
+
---
+++ pooja/Angular_Assignments/Assignment_A30(webAPI)/Assignment_A30(webAPI)/Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_A30_webAPI_.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Dob { get; set; }
+        public string Location { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+        public Employee(int id, string fname, string lname, string dob, string location,string email, string password)
+
+        {
+            this.Id = id;
+            this.FirstName = fname;
+            this.LastName = lname;
+            this.Dob = dob;
+            this.Location = location;
+            this.Email = email;
+            this.Password = password;
+        }
+        public Employee()
+        {
+
+        }
+    }
+}
