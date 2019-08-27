89020613dd127f1b569cf2fcd8a67b39e0ab280e
mukesh.jha <mukesh.jha>
2019-08-27 13:47:44 +0530

    Angular assignments

---
+++ Mukesh/Day22_Assignment28/ServiceWebAPI/ServiceWebAPI/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceWebAPI.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+
+        public Student(int id, string name,string course)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Course = course;
+        }
+    }
+}
---
+++ Mukesh/Day23_Assignment29/formValidation/formValidation/Files/ValidationScript.js
@@ -0,0 +1,14 @@
+﻿var myApp = angular.module("validateModule", [])
+    .controller("validateController", function ($scope) {
+
+        $scope.userName = ""
+        $scope.email = ""
+        $scope.password = ""
+        $scope.confirmPassword = ""
+        $scope.phone = ""
+        $scope.employees = [{ userName: "Rakesh", email: "R@r.com", password: "RJ98uik", confirmPassword: "RJ98uik", phone: "8973765309" }]
+
+        $scope.addDetails = function () {
+            $scope.employees.push({userName: $scope.userName, email: $scope.email, password: $scope.password, confirmPassword: $scope.password, phone: $scope.phone})
+        };
+    });
