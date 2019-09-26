e222b8d8c97880f88f3407243dd3727da981ec09
rachana <r.jadhav@globant.com>
2019-09-26 18:10:12 +0530

    Day 2 assignments added

---
+++ Rachana/Assignment3_Ctor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Assignment3_Ctor
+{
+    class Program
+    {
+        int redius;
+        static float pi;
+
+        static Program()    //static Constructor
+        {
+            pi = 3.14f;
+        }
+        public Program()  //default Constructor
+        {
+            redius = 35;
+
+        }
+        public Program(int r)  //Parameterized Constructor
+        {
+            redius = r;
+        }
+        public Program(Program program) //Copy Constructor
+        {
+            redius = program.redius;
+        }
+
+        public void calArea()
+        {
+            float area = pi * redius * redius;
+            Console.WriteLine("Area of Circle:{0}", area);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            p1.calArea();
+
+            Program p2 = new Program(25);
+            p2.calArea();
+
+            Program p3 = new Program(p1);
+            p3.calArea();
+
+        }
+    }
+}
---
+++ Rachana/Assignment4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Employee
+{
+    public class employee
+    {
+        public int emp_id;
+        public string emp_name;
+        public string emp_address;
+
+        public employee(int id, string name, string address)
+        {
+            this.emp_id = id;
+            this.emp_name = name;
+            this.emp_address = address;
+        }
+
+        public override string ToString()
+        {
+           return "emp_id:" + emp_id + "\temp_name:" + emp_name + "\temp_address:" + emp_address;
+        }
+       
+    }
+
+    class Genric_employee<T>
+    {
+        public List<T> list = new List<T>();
+        public void display()
+        {
+            try
+            {
+                foreach (var i in list)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("IndexOutOfRangeException Occured");
+            }
+        }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Genric_employee<employee> emp = new Genric_employee<employee>();
+
+            emp.list.Add(new employee(1, "Rachana", "Sangli"));
+            emp.list.Add(new employee(2, "Priyanka", "Pune"));
+            emp.list.Add(new employee(3, "Supriya", "Mumbai"));
+
+            emp.display();
+            emp.list.RemoveAt(0);
+            Console.WriteLine("Employee deleted");
+            emp.display();
+
+            Console.ReadKey();
+        }
+    }
+}
