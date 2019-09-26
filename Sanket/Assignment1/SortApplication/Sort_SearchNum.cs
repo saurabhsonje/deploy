75bac1c542ea3e727e5001f19eb1535bcd5c0e32
Sanket Ganjave <sanket.ganjave@globant.com>
2019-09-26 12:53:57 +0530

    Added following assignments, assignment 1: Sorting array and searching element from array assignment 2: Replacing string using StringBuilder

---
+++ Sanket/Assignment1/SortApplication/Sort_SearchNum.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Assignment01
+{
+    class Sort_SearchNum
+    {
+        static void AscSort(int[] arr) {
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+        static void DscSort(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+        static void AscSortUsingCompareTo(int[] arr) {
+            //Ascending Order
+            Array.Sort<int>(arr, new Comparison<int>(
+                (int a, int b) => a.CompareTo(b)
+                )) ;
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+        static void Find(int[] arr, int elem)
+        {
+            int pos = -1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == elem)
+                {
+                    pos = i;
+                    break;
+                }
+            }
+            if (pos >= 0)
+                Console.WriteLine("element found at index: " + pos);
+            else
+                Console.WriteLine("element is not found ");
+        }
+        static void Main(string[] args)
+        {
+            int[] arrnum = new int[]{5,2,1,3,5,6};
+
+            //Array.Sort(arrnum);
+            //Array.Reverse(arrnum);
+
+            //AscSortUsingCompareTo(arrnum);
+            Console.WriteLine("Array Elements :");
+            foreach (int item in arrnum)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.Write("\nAscending Sort :");
+            AscSort(arrnum);
+            foreach (int item in arrnum)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.Write("\nDescending Sort :");
+            DscSort(arrnum);
+            foreach (int item in arrnum)
+            {
+                Console.Write(item + " ");
+            }
+
+            int[] arr = new int[] { 1, 3, 4, 5, 6, 11, 12, 33 };
+            Console.WriteLine();
+            Console.WriteLine("\nArray elements :");
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            
+            
+            Console.WriteLine("\nEnter element to search from above array : ");
+            
+            Find(arr, Convert.ToInt32(Console.ReadLine()));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sanket/Assignment2/ReplaceStringSolution/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace ReplaceStringSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("this is text", 50);
+            
+            Console.WriteLine("Original text : "+ sb);
+            sb.Replace("this is", "replacement");
+            Console.WriteLine("replaced Text: " + sb);
+
+            StringBuilder sb2 = new StringBuilder("C# program");
+            
+            sb2.Append(" of Microsoft");
+            Console.WriteLine(sb2);
+            
+            sb2.Remove(10, 3);
+            Console.WriteLine(sb2);
+        }
+    }
+}
