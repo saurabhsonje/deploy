7d7d5c6f182855673f57f8375ee8c83a1bebf852
Sanket Ganjave <sanket.ganjave@globant.com>
2019-09-27 18:59:34 +0530

    Added assignment 3: constructor and usage. assignent 4: generic class of employee, add/delete functionality and exception handling.

---
+++ Sanket/Assignment3_4/EmployeeGenerics/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace EmployeeGenerics
+{
+    public class Employee<T>
+    {
+        private T name;
+        private T age; 
+        private T salary;
+        private T designation;
+        private static T id;
+
+        public Employee() { }
+        public Employee(T name, T age, T salary, T designation) {
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+            this.designation = designation;
+        }
+        public override string ToString()
+        {
+            Console.WriteLine();
+            return "ID: "+ this.PropertyId + "\nName: " + this.PropertyName + "\nAge: " + this.PropertyAge + "\nDesignation: " + this.PropertyDesg + "\nsalary: " + this.PropertySalary;
+        }
+
+        public T PropertyName { get { return name; } set { name = value; } }
+        public T PropertyAge { get { return age; } set { age = value; } }
+        public T PropertySalary { get { return salary; } set { salary = value; } }
+        public T PropertyDesg { get { return designation; } set { designation = value; } }
+        public T PropertyId { get { return id; } set { id = value; } }
+    }
+}
+
